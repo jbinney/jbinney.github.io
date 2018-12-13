4dd66d08f7937517656fe7d531476a016eb3e010
Joe Binney <joe@robinhood.com>
2018-12-13 14:25:31 -0800

    Python script to reload waitlist

---
+++ load_waitlist.py
@@ -0,0 +1,10 @@
+import urllib, json
+url = "https://api.robinhood.com/midlands/tailgate/mcduckling/spot/joe%40robinhood.com/"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+total = data.get("spot_number_total")
+print data
+html = '<html><body><div style="font-size: 200px">{:,}</div></body></html>'.format(total)
+file = open("waitlist.html", "w")
+file.write(html)
+file.close()
