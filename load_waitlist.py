54ac9158aba0258af082db0413b4c859a6ad70e9
Joe Binney <joe@robinhood.com>
2018-12-14 12:50:07 -0800

    Update waitlist

--- load_waitlist.py
+++ load_waitlist.py
@@ -4,7 +4,7 @@ response = urllib.urlopen(url)
 data = json.loads(response.read())
 total = data.get("spot_number_total")
 print data
-html = '<html><body><div style="font-size: 200px; text-align: center; margin-top: 400px">{:,}</div></body></html>'.format(total)
+html = '<html><body style="font-size: 200px; display: flex; align-items: center; justify-content: center"><div>{:,}</div></body></html>'.format(total)
 file = open("waitlist.html", "w")
 file.write(html)
 file.close()
