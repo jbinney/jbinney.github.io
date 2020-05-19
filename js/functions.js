b2251bdb95b5635dbfba3f408d9030aa068cecbc
Joe Binney <joseph.binney@gmail.com>
2020-05-19 17:30:17 -0500

    Rip out everything

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-function toggleVisible(id) {
-  elem = document.getElementById(id);
-  if (elem.style.display != 'block') {
-    elem.style.display = 'block';
-  } else {
-    elem.style.display = 'none';
-  }
-}
